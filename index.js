9b551c331b6c78bfb66c3c5b53ce5d189bba3f0a
kevin aviles <kevinaviles140@gmail.com>
2024-03-05 16:54:16 -0500

    commit array texto usandoel metodo

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let arrayDiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
 let arrayConstructor = new Array(3);
 let secuenciaNumerica = Array.from({ length: 5 }, (_, index) => index + 1);
 let arrayRepetido = Array(5).fill('a');
+let cadena = 'Hola, cómo estás';
+let arrayDesdeCadena = cadena.split(' ');
