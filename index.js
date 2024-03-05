ad8ea98b83b18ecd5fec34ca543abdc1d79e41b8
kevin aviles <kevinaviles140@gmail.com>
2024-03-05 17:11:34 -0500

    commit array genere valores dinamicante

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ let array2 = [4, 5, 6];
 let arrayCombinado = [...array1, ...array2];
 let arrayConValoresRepetidos = [1, 2, 2, 3, 3, 4, 5];
 let arrayUnico = [...new Set(arrayConValoresRepetidos)];
+function generarArrayDinamico(n) {
+    let array = [];
+    for (let i = 0; i < n; i++) {
+      array.push(Math.random());
+    }
+    return array;
+  }
+  
+  let arrayDinamico = generarArrayDinamico(5);
+  
