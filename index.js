09e131af82f1c28d94da295a3cfc510830477401
kevin aviles <kevinaviles140@gmail.com>
2024-03-05 16:48:13 -0500

    commit array valor utilizando el metodo

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let arrayConElementos = [1, 2, 3, 4, 5];
 let arrayDiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
 let arrayConstructor = new Array(3);
 let secuenciaNumerica = Array.from({ length: 5 }, (_, index) => index + 1);
+let arrayRepetido = Array(5).fill('a');
